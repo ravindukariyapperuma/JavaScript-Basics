c690fe94765b7ed5d8cfb9b041a31f5f8d745e30
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-18 23:39:52 +0530

    Array Functions example 7

--- ArrayFunctions - map() and fill()/sketch.js
+++ ArrayFunctions - map() and fill()/sketch.js
@@ -1,7 +1,10 @@
 let vals = [4, 8, 1, 2, 9];
+
+vals.fill(0);
+
 console.log(vals);
 
 
 
-vals = vals.map( (x) => x *2 );
-console.log(vals);
+// vals = vals.map( (x) => x *2 );
+// console.log(vals);
