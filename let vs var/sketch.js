89cceffc01c332770b36a084b6342296cf393fd1
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-18 15:51:06 +0530

    let vs var example 8

--- let vs var/sketch.js
+++ let vs var/sketch.js
@@ -2,6 +2,10 @@ function setup(){
     
     for (let i = 0; i < 100; i++){
         //some stuff happen
+        if (mouseX > 20) {
+            let j = 20;
+        }
+        console.log(j);
     }
 
     console.log(i);
