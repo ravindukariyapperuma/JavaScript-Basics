4e976880f7be435cdf821361b8343aca86e8f3c2
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-18 21:20:04 +0530

    arrow function example 6

--- Arrow Function/sketch.js
+++ Arrow Function/sketch.js
@@ -2,21 +2,26 @@ let counter1;
 
 function setup(){
     noCanvas();
-    counter1 = new Counter();
-}
-
-function draw() {
-    counter1.countIt();
+    console.log(this);
+    const counter1 = new Counter(100, 500);
+    const counter2 = new Counter(200, 700);
+    const counter3 = new Counter(500, 200);
 }
 
 class Counter {
-    constructor() {
-        this.count = 0;
+    constructor(start, wait) {
+        this.count = start;
         this.p = createP('');
+        console.log(this);
+        
+        setInterval( () => {
+            console.log(this);
+            this.count++;
+            this.p.html(this.count);
+        }, wait);
+
+        
     }
 
-    countIt() {
-        this.count++;
-        this.p.html(this.count);
-    }
+    
 }
