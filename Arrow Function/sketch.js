93e6a598d85e361c17889281fe8926b17b2f0ce7
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-18 20:59:37 +0530

    arrow function example 3

--- Arrow Function/sketch.js
+++ Arrow Function/sketch.js
@@ -2,7 +2,7 @@ function setup(){
     createCanvas(600, 400);
     background(0);
     let button = createButton('press');
-    button.mousePressed(function () {
+    button.mousePressed(() => {
         background(random(255));
     });
 
